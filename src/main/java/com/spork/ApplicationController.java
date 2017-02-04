6f28421ecc783db17c7eb12dd3b132bdef84ca43
Bryan Nguyen <b24nguye@uwaterloo.ca>
2017-02-04 16:05:09 -0500

    setup root for dummy

---
+++ src/main/java/com/spork/ApplicationController.java
@@ -0,0 +1,14 @@
+package com.spork;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+}
